ba2a26b092b2288ac226f0480414f56b448a82a2
Nghia Trung Nguyen <nghiatrung2112003@gmail.com>
2024-12-24 11:06:14 +0700

    feature/menu: UI for Menu component

--- news-app/src/app/admin/categories/page.tsx
+++ news-app/src/app/admin/categories/page.tsx
@@ -2,7 +2,7 @@ import CategoryManager from "@/app/components/CategoryManager";
 
 export default function Category() {
   return (
-    <main className="container mx-auto py-3 px-[100px]">
+    <main className="container mx-auto py-3 px-[300px]">
       <h1 className="text-3xl font-bold mb-2">Quản lý danh mục</h1>
       <CategoryManager />
     </main>
